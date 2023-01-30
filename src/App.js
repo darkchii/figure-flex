6d1346a5160e357d2f78a9d036156de41edfacfe
Mark Meijer <engineermarkpro@gmail.com>
2023-01-31 00:13:08 +0100

    Added figure data with photos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Container, createTheme, CssBaseline, Paper, ThemeProvider } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardHeader, CardMedia, Container, createTheme, CssBaseline, Grid, Paper, ThemeProvider, Typography } from "@mui/material";
 import Header from "./Header";
+import Figures from "./Figures/Figures.json";
 
 const darkTheme = createTheme({
   palette: {
@@ -8,13 +9,32 @@ const darkTheme = createTheme({
 });
 
 function App() {
+
+  const openFigureDetails = (figure) => {
+    console.log(figure);
+  }
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Container>
         <Header />
-        <Paper sx={{height: '500px'}}>
-
+        <Paper sx={{ p: 2 }}>
+          <Grid container spacing={2}>
+            {
+              Figures.map((figure) => {
+                return (
+                  <Grid item xs={12} sm={6} md={3} lg={2} xl={2}>
+                    <Card elevation={3}>
+                      <CardActionArea onClick={() => openFigureDetails(figure)}>
+                        <CardHeader titleTypographyProps={{variant: 'body2'}} title={figure.name} />
+                        <CardMedia component='img' height='196' image={figure.images[0].path} alt='Figure Thumbnail' />
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                )
+              })
+            }
+          </Grid>
         </Paper>
       </Container>
     </ThemeProvider>
